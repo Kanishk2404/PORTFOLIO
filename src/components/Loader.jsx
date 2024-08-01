d316e0616e0916519b966a615f69fbf07d5674e1
Kanishk2404 <114097334+Kanishk2404@users.noreply.github.com>
2024-08-01 17:10:38 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,26 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './Loader.css';
 
 const Loader = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCount((prevCount) => {
-        if (prevCount >= 100) {
-          clearInterval(interval);
-          document.body.classList.add('loading-complete'); // Add class here
+      setCount(prevCount => {
+        if (prevCount < 100) {
+          return prevCount + 1;
+        } else {
           return 100;
         }
-        return prevCount + 1;
       });
-    }, 20);
+    }, 50); // Adjust the speed of the countdown
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="loader-overlay">
-      <div className="loader-content">
-        <img src="https://64.media.tumblr.com/a4e6d07f4a448b3eb92585463baef425/68c9f2d62f7474c7-95/s1280x1920/8aa56dfca2eb93ef9159898a215b1dbc5b2ba135.jpg" alt="Loading..." className="anime-character" />
-        <div className="count">{count}%</div>
+      <div className="loader">
+        <div className="countdown">{count}%</div>
       </div>
     </div>
   );
